fd7e607f8f0f28206ead76f39d9a53380f90a654
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2023-12-06 20:55:38 +0900

    Apply format

--- ffmpeg_sendnode/src/send_image.cpp
+++ ffmpeg_sendnode/src/send_image.cpp
@@ -5,51 +5,47 @@
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 
-
 Transdata transdata;
 std::mutex mImage_buf;
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
-    ros::init(argc,argv,"talker");
+  ros::init(argc, argv, "talker");
 
-    ros::NodeHandle n;
+  ros::NodeHandle n;
 
-    image_transport::ImageTransport it(n);
-    image_transport::Publisher pub = it.advertise("camera/image", 1);
-    ros::Rate loop_rate(10);
+  image_transport::ImageTransport it(n);
+  image_transport::Publisher pub = it.advertise("camera/image", 1);
+  ros::Rate loop_rate(10);
 
-    int count = 0;
-    sensor_msgs::ImagePtr msg;
+  int count = 0;
+  sensor_msgs::ImagePtr msg;
 
-    if(transdata.Transdata_init() < 0)
+  if (transdata.Transdata_init() < 0)
+  {
+    cout << "init error !" << endl;
+    return -1;
+  }
+
+  while (ros::ok())
+  {
+    //接收图像并显示
+    transdata.Transdata_Recdata();
+
+    //需要使用互斥锁 因为要操作图像数据
+    mImage_buf.lock();
+    if (!transdata.image_test.empty())
     {
-        cout <<"init error !" << endl;
-        return -1;
+      //            imshow("test",transdata.image_test);
+      //            waitKey(10);
+      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", transdata.image_test).toImageMsg();
+      pub.publish(msg);
+      cout << " send image " << count << endl;
+      count++;
+      transdata.image_test.release();
     }
+    mImage_buf.unlock();
 
-    while(ros::ok())
-    {
-        //接收图像并显示
-        transdata.Transdata_Recdata();
-
-        //需要使用互斥锁 因为要操作图像数据
-        mImage_buf.lock();
-        if(!transdata.image_test.empty())
-        {
-//            imshow("test",transdata.image_test);
-//            waitKey(10);
-            msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", transdata.image_test).toImageMsg();
-            pub.publish(msg);
-            cout << " send image " << count << endl;
-            count ++;
-            transdata.image_test.release();
-        }
-        mImage_buf.unlock();
-
-        ros::spinOnce();
-    }
-    return 0;
+    ros::spinOnce();
+  }
+  return 0;
 }
-
-
-
